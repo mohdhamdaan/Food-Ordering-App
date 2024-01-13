9e69dc990f4ce1f46fcbb5b49a50949ab3da99ca
Hamdaan <you@example.com>
2024-01-13 20:11:10 +0530

    made changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import { LOGO_URL } from "../utils/constants";
-import { useState , useContext} from "react";
+import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
 import { useSelector } from "react-redux";
+import {FaShoppingCart} from "react-icons/fa"
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
   const onlineStatus = useOnlineStatus();
-  const {loggedInUser} = useContext(UserContext);
+  const { loggedInUser } = useContext(UserContext);
   const cartItems = useSelector((store) => store.cart.items);
   return (
     <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
@@ -17,11 +18,22 @@ const Header = () => {
       </div>
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
-          <li className="px-4">Online Status: {onlineStatus? "online" : "offline"}</li>
-          <li className="px-4"><Link to="/">Home</Link></li>
-          <li className="px-4"><Link to="/about">About</Link></li>
-          <li className="px-4"><Link to="/contact">Contact Us</Link></li>
-          <li className="px-4 font-bold text-xl"><Link to="/cart">Cart({cartItems.length})</Link></li>
+          <li className="px-4">Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li className="px-2 text-2xl text-black">
+            <Link to="/cart" className="flex align-middle">
+              <FaShoppingCart value={{ className: "bg-c" }} />
+              <span className="px-4 -m-2">-({cartItems.length})</span>
+            </Link>
+          </li>
           <button
             className="login"
             onClick={() => {
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,39 +3,68 @@ import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
 import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { CDN_URL } from "../utils/constants";
+import MenuShimmer from "./MenuShimmer";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const resInfo = useRestaurantMenu(resId);
   const [showIndex, setShowIndex] = useState(null);
 
-  if (resInfo === null) return <Shimmer />;
-  const { name, cuisines, costForTwoMessage } = resInfo.cards[0].card.card.info;
-  //   const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-
-    // console.log(resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+  if (resInfo === null) return <MenuShimmer />;
+  const { name, cuisines, costForTwoMessage,cloudinaryImageId, areaName, locality, avgRating } = resInfo.cards[0].card.card.info;
+  const { deliveryTime } = resInfo?.cards[0]?.card?.card?.info?.sla;
   const categories =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
       (c) =>
         c.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
-
+  const toggleShowIndex = (index) => {
+    if (showIndex === index) {
+      setShowIndex(null);
+    } else {
+      setShowIndex(index);
+    }
+  };
   return (
-    <div className="text-center">
-      <h1 className="font-bold my-6 text-2xl">{name}</h1>
-      <p className="font-bold text-lg">
-        {cuisines.join(",")} - {costForTwoMessage}
-      </p>
+    <div>
+    <div className="flex mx-auto w-6/12">
+      <img
+        className="py-4 rounded-lg w-60 h-40"
+        src={CDN_URL + cloudinaryImageId}
+        alt="restaurant-logo"
+      />
+      <div>
+        <div>
+          <h1 className="pt-4 pl-2 font-bold text-3xl">{name}</h1>
+          <div className="pl-2 mt-1">
+            <p>{cuisines.join(",")}</p>
+          </div>
+          <div className="pl-2">
+            <p>{`${areaName},${locality}`}</p>
+          </div>
+          <div className="h-6/12 flex mt-3 pl-2">
+            <div className="w-2/12 flex text-lg font-bold">
+              <span>{avgRating}</span>
+              <span className="pl-1 mt-1">{<FaStar />}</span>
+            </div>
+            <span> &emsp;| </span>{" "}
+            
+            <div className="pl-2 w-9/12">{costForTwoMessage}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
       {categories.map((category, index) => (
         <RestaurantCategory
           key={category?.card?.card.title}
           data={category?.card?.card}
           showItems={index === showIndex ? true : false}
-          setShowIndex={() =>
-            setShowIndex((prevIndex) => (prevIndex === index ? null : index))
-          }
+          setShowIndex={() => toggleShowIndex(index)}
         />
       ))}
     </div>
